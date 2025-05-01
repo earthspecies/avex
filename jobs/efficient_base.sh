a2ac05150f30e70f8b95c708088038132d0bc16e
Milad Alizadeh <git@mil.ad>
2025-05-01 10:46:48 +0100

    Added the first slurm job script

---
+++ jobs/efficient_base.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#SBATCH --partition=a100-40
+#SBATCH --gpus=1
+#SBATCH --ntasks-per-gpu=1
+#SBATCH --output="/home/%u/logs/%A.log"
+#SBATCH --job-name="representation learning"
+
+cd ~/representation-learning
+uv sync
+srun uv run representation_learning/run_train.py --config configs/run_configs/efficientnet_base.yml
