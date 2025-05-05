cb33df985d0f67eaa26d6bedd35d2bb8a303a601
Name <miron.marius@gmail.com>
2025-05-05 17:02:07 +0200

    added parameters for the evaluation

--- esp_data_temp/config.py
+++ esp_data_temp/config.py
@@ -18,3 +18,5 @@ class DataConfig(BaseModel):
     label_type: str
     # TODO (milad) what is dc_field? 🤔
     read_csv_kwargs: Dict[str, Any] = dc_field(default_factory=dict)
+    multi_label: Optional[bool] = None
+    sample_rate: Optional[int] = None  # Sample rate for audio data
