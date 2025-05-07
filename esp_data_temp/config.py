d83123be3f9e155eaf87c8a811294656eff227c0
Marius Miron <miron.marius@gmail.com>
2025-05-07 12:42:59 +0200

    Update esp_data_temp/config.py
    Co-authored-by: Milad Alizadeh <686449+mil-ad@users.noreply.github.com>

--- esp_data_temp/config.py
+++ esp_data_temp/config.py
@@ -18,5 +18,5 @@ class DataConfig(BaseModel):
     label_type: str
     # TODO (milad) what is dc_field? 🤔
     read_csv_kwargs: Dict[str, Any] = dc_field(default_factory=dict)
-    multi_label: Optional[bool] = None
+    multi_label: bool | None = None
     sample_rate: Optional[int] = None  # Sample rate for audio data
