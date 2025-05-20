971ffdbc7c06fa190f4391269c17f8333bdad1eb
Cheeky Sparrow <ck@ck.com>
2025-05-20 14:23:53 +0100

    fix init issues

--- esp_data_temp/registered_datasets/__init__.py
+++ esp_data_temp/registered_datasets/__init__.py
@@ -1,6 +1,18 @@
 """Registered datasets package."""
 
-from .registry import DatasetInfo, register_dataset, registry
+from esp_data_temp.dataset import (
+    DatasetInfo,
+    list_registered_datasets,
+    print_registered_datasets,
+    register_dataset,
+)
+
 from .animalspeak import AnimalSpeak
 
-__all__ = ["DatasetInfo", "register_dataset", "registry", "AnimalSpeak"]
+__all__ = [
+    "DatasetInfo",
+    "register_dataset",
+    "list_registered_datasets",
+    "print_registered_datasets",
+    "AnimalSpeak",
+]
