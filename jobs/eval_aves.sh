f418bb9e82f23dc20fe884337a6a0d8c7159ca2e
Name <miron.marius@gmail.com>
2025-05-21 19:52:30 +0200

    aves job script

---
+++ jobs/eval_aves.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#SBATCH --partition=a100-40
+#SBATCH --gpus=1
+#SBATCH --ntasks-per-gpu=1
+#SBATCH --output="/home/%u/logs/%A.log"
+#SBATCH --job-name="clip eval 5e5"
+
+cd ~/representation-learning
+uv sync
+srun uv run representation_learning/run_evaluate.py --config configs/evaluation_configs/aves_bio.yml
