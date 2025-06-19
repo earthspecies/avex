896eac48fc9270c2b7c22f2cb9eb63d54384fce0
Name <miron.marius@gmail.com>
2025-06-19 09:57:53 +0200

    script

--- jobs/efficientnet_small.sh
+++ jobs/efficientnet_small.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-#SBATCH --partition=debug
+#SBATCH --partition=a100-40
 #SBATCH --gpus=1
 #SBATCH --ntasks-per-gpu=1
 #SBATCH --output="/home/%u/logs/%A.log"
 #SBATCH --job-name="rl-effnet-test"
 #SBATCH --cpus-per-gpu=12
+#SBATCH --mem=32G  # Reserve 32GB of RAM
 
 cd ~/representation-learning
 uv sync
