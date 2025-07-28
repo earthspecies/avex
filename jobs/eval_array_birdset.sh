153b61b80a794abd59397e8c3169f2211b51db6e
Name <miron.marius@gmail.com>
2025-07-28 10:25:45 -0400

    comment ssw

--- jobs/eval_array_birdset.sh
+++ jobs/eval_array_birdset.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
 
-#SBATCH --array=1-7%3
+#SBATCH --array=1-6%3
 #SBATCH --partition=a100-40
 #SBATCH --gpus=1
 #SBATCH --ntasks-per-gpu=1
 #SBATCH --output="/home/%u/logs/%A_%a.log"
 #SBATCH --job-name="rl-eval-array"
 #SBATCH --cpus-per-gpu=10
-#SBATCH --mem-per-gpu=64G
 
 # Map array task ID to config file
 declare -A configs=(
-    # [1]="efficientnet_beans.yml"
-    # [2]="eat_hf_bio.yml"
-    # [3]="eat_hf_audioset.yml"
-    # [4]="eat_hf_all.yml"
-    # [5]="sl_efficientnet_audioset.yml"
-    [6]="efficientnet_bio_birdset.yml"
-    # [7]="sl_efficientnet_all.yml"
-    # [3]="atst_frame.yml"
-    # [4]="clap.yml"
-    # [5]="beats.yml"
-    # [6]="bird_aves_bio.yml"
-    # [7]="beats_naturelm.yml"
-    # [8]="beats_finetuned.yml"
-    # [9]="perch.yml"
+    [1]="sl_efficientnet_audioset.yml"
+    [2]="sl_efficientnet_animalspeak.yml"
+    [3]="sl_efficientnet_animalspeak_audioset.yml"
+    [4]="ssl_eat_all.yml"
+    [5]="ssl_eat_animalspeak.yml"
+    [6]="ssl_eat_audioset.yml"
+    [7]="atst_frame.yml"
+    [8]="clap.yml"
+    [9]="beats.yml"
+    [10]="bird_aves_bio.yml"
+    [11]="beats_naturelm.yml"
+    [12]="beats_finetuned.yml"
+    [13]="perch.yml"
 )
 
 # Get the config file for this array task
@@ -37,10 +35,10 @@ fi
 
 echo "Running evaluation for model: $config_file (Task ID: $SLURM_ARRAY_TASK_ID)"
 
+cd ~/representation-learning
 uv tool install keyring --with keyrings.google-artifactregistry-auth
+export UV_PROJECT_ENVIRONMENT=/scratch/$USER/venvs/
 export GOOGLE_APPLICATION_CREDENTIALS=/home/marius_miron_earthspecies_org/.config/gcloud/application_default_credentials.json
 export CLOUDPATHLIB_FORCE_OVERWRITE_FROM_CLOUD=1
-
-cd ~/representation-learning
 uv sync
 srun uv run repr-learn evaluate --config configs/evaluation_configs/single_models_birdset/$config_file
