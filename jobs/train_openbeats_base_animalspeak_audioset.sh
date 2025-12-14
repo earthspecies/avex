48065b65e5bfaea9738959d6c1595229909c3ec8
chrispla <cplachouras@gmail.com>
2025-12-14 17:54:34 +0000

    openbeats ft scripts and configs

---
+++ jobs/train_openbeats_base_animalspeak_audioset.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#SBATCH --partition=a100-40
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=12
+#SBATCH --output="/home/%u/logs/%A.log"
+#SBATCH --job-name="train-openbeats-base-anispk"
+
+set -euo pipefail
+source ~/slurm_env
+
+cd ~/representation-learning
+uv sync --group project-dev
+
+CONFIG=${1:-"configs/run_configs/aaai_train/sl_openbeats_base_animalspeak_audioset.yml"}
+
+srun uv run repr-learn train --config "${CONFIG}"
+
